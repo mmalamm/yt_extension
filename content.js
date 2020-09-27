7a51c4688109b5480aa8bed0058d492e16c226f2
M M Alam <alam@mmtutor.com>
2020-09-27 02:43:21 -0400

    add speed up and slow down to f5, f6 keys

--- content.js
+++ content.js
@@ -1,9 +1,21 @@
-const video = document.querySelector("video");
+console.log("loading yt_extension...");
+
+const getCurrentVid = () => document.querySelector("video");
 
 navigator.mediaSession.setActionHandler("previoustrack", () => {
+  const video = getCurrentVid();
   video.currentTime = video.currentTime - 5;
 });
 
 navigator.mediaSession.setActionHandler("nexttrack", function () {
+  const video = getCurrentVid();
   video.currentTime = video.currentTime + 5;
+});
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "F6") {
+    getCurrentVid().playbackRate += 0.25;
+  } else if (e.key === "F5") {
+    getCurrentVid().playbackRate -= 0.25;
+  }
 });
