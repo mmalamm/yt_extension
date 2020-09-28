ef577be28f139fb0c77f5d2eb159e8097c76344e
M M Alam <alam@mmtutor.com>
2020-09-28 09:55:25 -0400

    add stuff

--- content.js
+++ content.js
@@ -2,20 +2,22 @@ console.log("loading yt_extension...");
 
 const getCurrentVid = () => document.querySelector("video");
 
-navigator.mediaSession.setActionHandler("previoustrack", () => {
-  const video = getCurrentVid();
-  video.currentTime = video.currentTime - 5;
-});
+navigator.mediaSession.setActionHandler(
+  "previoustrack",
+  () => (getCurrentVid().currentTime -= 5)
+);
 
-navigator.mediaSession.setActionHandler("nexttrack", function () {
-  const video = getCurrentVid();
-  video.currentTime = video.currentTime + 5;
-});
+navigator.mediaSession.setActionHandler(
+  "nexttrack",
+  () => (getCurrentVid().currentTime += 5)
+);
 
 document.addEventListener("keydown", (e) => {
   if (e.key === "F6") {
     getCurrentVid().playbackRate += 0.25;
   } else if (e.key === "F5") {
-    getCurrentVid().playbackRate -= 0.25;
+    const vid = getCurrentVid();
+    if (vid.playbackRate === 0) return;
+    vid.playbackRate -= 0.25;
   }
 });
